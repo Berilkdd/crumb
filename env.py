426d758fdbaee81d2a697f7886692989574b529c
Berilkdd <brlkrdnz@hotmail.com>
2026-06-25 13:43:43 +0100

    Replace compromised Google Maps API key and move it to environment variables

--- env.py
+++ env.py
@@ -3,3 +3,4 @@ import os
 os.environ["SECRET_KEY"] = "sd81fc6c616fb9120b0e89bcf996d2a8094aac7b2422c4662276e795a5373ba84"
 os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_si8fXjxUM1KR@ep-patient-truth-agst2yfl.c-2.eu-central-1.aws.neon.tech/zippy_atlas_error_570126"
 os.environ.setdefault('DEVELOPMENT', '1')
+os.environ["GOOGLE_MAPS_API_KEY"] = "AIzaSyBDG9_sHYYUWnN_5-AMbW9AoeXM2chm84o"
--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+import os
 
 def home(request):
     """ A view to return home page """
-    return render(request, 'home/home.html')
+
+    context = {
+        "google_maps_api_key": os.environ["GOOGLE_MAPS_API_KEY"],
+    }
+
+    return render(request, "home/home.html", context)
