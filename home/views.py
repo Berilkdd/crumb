dc1be692a64a6abf94607f26ff526290c6aec6b0
Berilkdd <brlkrdnz@hotmail.com>
2026-06-25 15:05:01 +0100

    Add manu page with styling

--- home/views.py
+++ home/views.py
@@ -3,9 +3,11 @@ import os
 
 def home(request):
     """ A view to return home page """
-
     context = {
         "google_maps_api_key": os.environ["GOOGLE_MAPS_API_KEY"],
     }
-
     return render(request, "home/home.html", context)
+
+def menu(request):
+    """A view to return menu page"""
+    return render(request, "home/menu.html")
