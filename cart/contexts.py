3d91b13143a4147dc87458eec9df123d46814895
Berilkdd <brlkrdnz@hotmail.com>
2026-06-26 21:55:08 +0100

    Add session cart functionality and dynamic cart badge

---
+++ cart/contexts.py
@@ -0,0 +1,11 @@
+def cart_contents(request):
+
+    cart = request.session.get("cart", {})
+
+    product_count = sum(cart.values())
+
+    context = {
+        "product_count": product_count,
+    }
+
+    return context
--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
 
-def cart(request):
+
+def add_to_cart(request, item_id):
+
+    product = get_object_or_404(Product, pk=item_id)
+
+    cart = request.session.get("cart", {})
+
+    item_id = str(item_id)
+
+    if item_id in cart:
+        cart[item_id] += 1
+    else:
+        cart[item_id] = 1
+
+    request.session["cart"] = cart
+
+    print(request.session["cart"])
+
+    return redirect("products")
+
+def view_cart(request):
     return render(request, "cart/cart.html")
