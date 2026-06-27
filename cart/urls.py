5ae752a13438c5dfddeb66784bd220057a086d77
Berilkdd <brlkrdnz@hotmail.com>
2026-06-27 01:38:55 +0100

    Add cart list to cart page

--- cart/urls.py
+++ cart/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("add/<int:item_id>/", views.add_to_cart, name="add_to_cart"),
+    path("remove/<int:item_id>/", views.remove_from_cart, name="remove_from_cart"),
+    path("increase/<int:item_id>/", views.increase_quantity, name="increase_quantity"),
+    path("decrease/<int:item_id>/", views.decrease_quantity, name="decrease_quantity"),
     path("", views.view_cart, name="view_cart"),
 ]
--- cart/views.py
+++ cart/views.py
@@ -22,4 +22,65 @@ def add_to_cart(request, item_id):
     return redirect("products")
 
 def view_cart(request):
-    return render(request, "cart/cart.html")
+
+    cart = request.session.get("cart", {})
+
+    cart_items = []
+
+    for item_id, quantity in cart.items():
+
+        product = get_object_or_404(Product, pk=item_id)
+
+        cart_items.append({
+            "product": product,
+            "quantity": quantity,
+        })
+
+    context = {
+        "cart_items": cart_items,
+    }
+
+    return render(request, "cart/cart.html", context)
+
+def remove_from_cart(request, item_id):
+
+    cart = request.session.get("cart", {})
+
+    item_id = str(item_id)
+
+    if item_id in cart:
+        del cart[item_id]
+
+    request.session["cart"] = cart
+
+    return redirect("view_cart")
+
+def increase_quantity(request, item_id):
+
+    cart = request.session.get("cart", {})
+
+    item_id = str(item_id)
+
+    if item_id in cart:
+        cart[item_id] += 1
+
+    request.session["cart"] = cart
+
+    return redirect("view_cart")
+
+def decrease_quantity(request, item_id):
+
+    cart = request.session.get("cart", {})
+
+    item_id = str(item_id)
+
+    if item_id in cart:
+
+        if cart[item_id] > 1:
+            cart[item_id] -= 1
+        else:
+            del cart[item_id]
+
+    request.session["cart"] = cart
+
+    return redirect("view_cart")
