7148188c4e0e77004d1270cf973f158997128a6c
Berilkdd <brlkrdnz@hotmail.com>
2026-07-01 00:14:41 +0100

    Configure allowed hosts for deployment

--- crumb/settings.py
+++ crumb/settings.py
@@ -29,8 +29,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".herokuapp.com",
+]
 
 # Application definition
 
