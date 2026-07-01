a4a06deacdfdb5247b121167a0f4c3b0c9bd32ce
Berilkdd <brlkrdnz@hotmail.com>
2026-07-01 09:24:54 +0100

    all_products view deployment check

--- products/views.py
+++ products/views.py
@@ -6,10 +6,11 @@ from .models import Product
 def all_products(request):
     products = Product.objects.all()
 
-    return render(
-        request,
-        "products/products.html",
-        {
-            "products": products,
-        },
-    )
+    try:
+        return render(
+            request,
+            "products/products.html",
+            {"products": products},
+        )
+    except Exception as e:
+        return HttpResponse(f"{type(e).__name__}: {e}")
