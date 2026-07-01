5c3ea86abf5ea0a063ec2afac932f05825c2a0a1
Berilkdd <brlkrdnz@hotmail.com>
2026-07-01 08:18:13 +0100

    Products deployment test

--- products/views.py
+++ products/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render
-from .models import Product
+##from django.shortcuts import render
+##from .models import Product
 
+
+##def all_products(request):
+##    """A view to display all products."""
+
+##    products = Product.objects.all()
+
+##    context = {
+##        'products': products,
+##    }
+
+##    return render(request, 'products/products.html', context)
+
+
+from django.http import HttpResponse
 
 def all_products(request):
-    """A view to display all products."""
-
-    products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'products/products.html', context)
+    return HttpResponse("Products OK")
