b43d55cd825bdd4dbabd3579ca1bf577c190ed77
Berilkdd <brlkrdnz@hotmail.com>
2026-07-01 08:31:46 +0100

    Products template image section deployment check

--- products/views.py
+++ products/views.py
@@ -1,20 +1,14 @@
-##from django.shortcuts import render
-##from .models import Product
+from django.shortcuts import render
+from .models import Product
 
-
-##def all_products(request):
-##    """A view to display all products."""
-
-##    products = Product.objects.all()
-
-##    context = {
-##        'products': products,
-##    }
-
-##    return render(request, 'products/products.html', context)
-
-
-from django.http import HttpResponse
 
 def all_products(request):
-    return HttpResponse("Products OK")
+    """A view to display all products."""
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
